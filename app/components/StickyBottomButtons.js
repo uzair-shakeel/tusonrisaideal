4b489508ced9466799e1f7e7b048a0ed7ddbcd49
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-07-18 23:33:10 +0500

    ..

--- app/components/StickyBottomButtons.js
+++ app/components/StickyBottomButtons.js
@@ -4,7 +4,7 @@ import { MdPhone } from "react-icons/md";
 const StickyBottomButtons = () => {
   return (
     <div>
-      <div className="fixed py-4 w-full z-50 bottom-0 bg-white grid grid-cols-2 gap-[13px] px-3 leading-[17px] text-[14px]">
+      <div className="fixed py-4 w-full z-50 bottom-0 bg-white grid grid-cols-2 lg:hidden gap-[13px] px-3 leading-[17px] text-[14px]">
         <button className="py-3 flex gap-2 items-center justify-center px-4 border border-black text-black font-bold rounded-[10px]">
           <MdPhone size={18} /> Call
         </button>
